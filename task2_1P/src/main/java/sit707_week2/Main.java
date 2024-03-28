1279f47d674629200b434eecdcb6d977411273f9
Dev Patel <dev2462000@gmail.com>
2024-03-28 17:53:29 +1100

    final

---
+++ task2_1P/src/main/java/sit707_week2/Main.java
@@ -0,0 +1,42 @@
+package sit707_week2;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ * @author DEV PATEL
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        /*try {
+         
+			SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+    	try {
+            // Choose which operation to execute
+            int choice = 1; // Change this to 1 or 2 based on your preference
+            
+            if (choice == 1) {
+                // Execute operation for Officeworks registration page
+                SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
+            } else if (choice == 2) {
+                // Execute operation for Parabank registration page
+                SeleniumOperations.Parabank_registration_page("https://parabank.parasoft.com/parabank/register.htm");
+            } else {
+                System.out.println("Invalid choice.");
+            }
+        } catch (IOException e) {
+            // Print stack trace if an IOException occurs
+            e.printStackTrace();
+        }
+        
+    }
+    
+    
+}
