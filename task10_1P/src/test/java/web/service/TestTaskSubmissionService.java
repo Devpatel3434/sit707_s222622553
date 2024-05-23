55eb8dd3add84cbc6a89a76ffac0a967922543f6
Dev Patel <dev2462000@gmail.com>
2024-05-23 10:44:46 +1000

    fail

---
+++ task10_1P/src/test/java/web/service/TestTaskSubmissionService.java
@@ -0,0 +1,18 @@
+package web.service;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import web.TaskSubmissionService;
+
+public class TestTaskSubmissionService {
+	
+	@Test
+    public void testSubmitTask() {
+        TaskSubmissionService TaskSubmissionService = new TaskSubmissionService();
+        boolean result = TaskSubmissionService.submitTask("S222622553", "Task34");
+        assertTrue(result);
+    }
+    
+
+}
