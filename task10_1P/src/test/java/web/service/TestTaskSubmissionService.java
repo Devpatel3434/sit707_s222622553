c8c06612c4610fffeb5a04012ea903303c764a17
Dev Patel <dev2462000@gmail.com>
2024-05-23 11:45:06 +1000

    Pass

--- task10_1P/src/test/java/web/service/TestTaskSubmissionService.java
+++ task10_1P/src/test/java/web/service/TestTaskSubmissionService.java
@@ -13,6 +13,19 @@ public class TestTaskSubmissionService {
         boolean result = TaskSubmissionService.submitTask("S222622553", "Task34");
         assertTrue(result);
     }
+	
+    @Test
+    public void testSubmitTaskWithNullValues() {
+        TaskSubmissionService TaskSubmissionService = new TaskSubmissionService();
+        boolean result = TaskSubmissionService.submitTask(null, null);
+        assertFalse(result);
+    }
     
- 
+    @Test
+    public void testSubmitTaskWithEmptyValues() {
+        TaskSubmissionService TaskSubmissionService = new TaskSubmissionService();
+        boolean result = TaskSubmissionService.submitTask("", "");
+        assertFalse(result);
+    }
+
 }
